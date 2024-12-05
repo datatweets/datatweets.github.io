9d4395ccb6b4a20730a6db91ff9bdd4aa1993f56
mehdi <lotfinejad@gmail.com>
2024-12-05 11:23:28 +0800

    update

--- content/_index.md
+++ content/_index.md
@@ -1,13 +1,13 @@
 ---
-title: "Welcome to Doks"
+title: "Welcome to Datatweets"
 description: ""
-lead: "Congrats on setting up a new Doks project!"
+lead: "Learn Data Simplified!"
 date: 2023-09-07T16:33:54+02:00
 lastmod: 2023-09-07T16:33:54+02:00
 draft: false
 seo:
-  title: "Welcome to Doks" # custom title (optional)
-  description: "" # custom description (recommended)
+  title: "Welcome to Datatweets" # custom title (optional)
+  description: "Learn Data Simplified" # custom description (recommended)
   canonical: "" # custom canonical URL (optional)
   robots: "" # custom robot tags (optional)
 ---
--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,22 @@ hugo --minify
 # Navigate to the public folder
 cd public
 
-# Add and commit changes
-git init
+# Initialize Git if it doesn't exist
+if [ ! -d ".git" ]; then
+  git init
+  git remote add main https://github.com/datatweets/datatweets.github.io.git
+fi
+
+# Pull the latest changes to avoid conflicts and preserve the CNAME file
+git fetch main gh-pages
+git reset --soft main/gh-pages
+
+# Add and commit the new changes
 git add .
 git commit -m "Update site content"
 
-# Push to gh-pages
+# Push to the gh-pages branch
 git branch -M gh-pages
-git remote add main https://github.com/datatweets/datatweets.github.io.git
 git push -u main gh-pages --force
 
 # Return to the project root
